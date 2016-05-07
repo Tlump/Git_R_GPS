cabc2326e94c678509384fa03bbc58a139437950
Lumpsum Tongsinoot <TLump_MAC@Macintosh-6.local>
2016-05-07 20:11:44 +0700

    Revert "Cal time Diff all day"
    This reverts commit 9f47f89d31b2b81704be9e32a786b98763835203.

--- 2_car_Timediff_Trip.R
+++ 2_car_Timediff_Trip.R
@@ -1,86 +1,70 @@
-#20160506
-#cal time all day
+# 20160504
+# cal timediff in same day
+#bug drive car in midnight
 
 library("parallel")
 #Create cluster
 clus <- makeCluster(3)
 
 #Option 1. Declare the function for each node
-clusterEvalQ(clus,clusterTrip <- function(CarID,x){
-                    # select data by carID
-                    select <- car[car$CarID==CarID,]
+clusterEvalQ(clus,clusterTrip <- function(CarID,Date){
+                    select <- car[car$CarID==CarID & car$Date==Date,]
                     select <- select[order(select$Time.Sec),]
-                    # init column
-                    select$timediff <- -2 
+                    # get timediff
+                    select$timediff <- c(0, diff(select$Time.Sec))
+                    select[1,]$timediff = -1
+                    select[nrow(select),]$timediff = -2
+                    # get TRIP
                     select$trip <- 0
-                    select$tripStatus <- "-"
-                    #get trip
-                    tripId<-1
-                    select[1,]$trip <- tripId
-                    select[1,]$timediff <- -1
-                    select[1,]$tripStatus <- "O"
-                    select[nrow(select),]$tripStatus <- "D"
-                    
-                    for(i in 2:nrow(select)){
-                      select[i,]$timediff <- select[i,]$Time.Sec - select[(i-1),]$Time.Sec
-                      # extract Trip using timediff 
-                      if(select[i,]$timediff>=600){
-                        tripId <- tripId+1
-                        select[(i-1),]$tripStatus <- "D"
-                        select[(i),]$tripStatus <- "O"
+                    j<-1
+                    for(i in 1:nrow(select)){
+                      select[i,]$trip <- j
+                      if(select[i,]$timediff>=600 || select[i,]$timediff==-2){
+                        j <- j+1
                       }
-                      select[i,]$trip <- tripId
                     }
                     return(select)
                   }
 )
 
 #Option 2. Export it form base workspace
-clusterTrip <- function(CarID,x){
-  # select data by carID
- select <- car[car$CarID==CarID,]
- select <- select[order(select$Time.Sec),]
-  # init column
-  select$timediff <- -2 
+clusterTrip <- function(CarID,Date){
+  select <- car[car$CarID==CarID & car$Date==Date,]
+  select <- select[order(select$Time.Sec),]
+  # get timediff
+  select$timediff <- c(0, diff(select$Time.Sec))
+  select[1,]$timediff = -1
+  select[nrow(select),]$timediff = -2
+  # get TRIP
   select$trip <- 0
-  select$tripStatus <- "-"
-  #get trip
-  tripId<-1
-  select[1,]$trip <- tripId
-  select[1,]$timediff <- -1
-  select[1,]$tripStatus <- "O"
-  select[nrow(select),]$tripStatus <- "D" 
- 
-  for(i in 2:nrow(select)){
-    select[i,]$timediff <- select[i,]$Time.Sec - select[(i-1),]$Time.Sec
-    # extract Trip using timediff 
-    if(select[i,]$timediff>=600){
-      tripId <- tripId+1
-      select[(i-1),]$tripStatus <- "D"
-      select[(i),]$tripStatus <- "O"
+  j<-1
+  for(i in 1:nrow(select)){
+    select[i,]$trip <- j
+    if(select[i,]$timediff>=600 || select[i,]$timediff==-2){
+      j <- j+1
     }
-    select[i,]$trip <- tripId
   }
   return(select)
 }
 clusterExport(clus,"clusterTrip")
 clusterExport(clus,"car")
 
-# main
-car.clus <- do.call(rbind,parRapply(clus,car.count[,c("CarID","x")],function(row) clusterTrip(row["CarID"],row["x"])))
 
-# Finish
-stopCluster(clus)
+# main
 
 # select car
 carID.df <- plyr::count(car, c("CarID"))
-carID.df <- carID.df[1:200,] 
+carID.df <- carID.df[1:50,] 
 carID <- carID.df$CarID
 car <- car[car$CarID %in% carID , ]
 car <- car[order(car$CarID,car$Time.Sec),]
- car.count <- plyr::count(car, c("CarID"))
-car.count$x <- 0
+car.count <- plyr::count(car, c("CarID","Date"))
 rm(carID)
 rm(carID.df)
+
+car.clus <- do.call(rbind,parRapply(clus,car.count[,c("CarID","Date")],function(row) clusterTrip(row["CarID"],row["Date"])))
+
+# Finish
+stopCluster(clus)
 
 write.csv(car.clus, file = "/Users/TLump_MAC/Thesis/Data/temp/car_clus.csv")
